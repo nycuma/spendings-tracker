e23f814db96f702229890f54a123a17a73c5b688
Juri <31799327+nycuma@users.noreply.github.com>
2018-09-08 21:50:19 +0200

    some test data

---
+++ src/utils/ExampleData.js
@@ -0,0 +1,56 @@
+let spendings = [
+    {
+        day: new Date(2018, 8, 5),
+        cat: 'food',
+        amount: 5.43
+    },
+    {
+        day: new Date(2018, 8, 5),
+        cat: 'food',
+        amount: 10.20
+    },
+    {
+        day: new Date(2018, 8, 5),
+        cat: 'food',
+        amount: 3
+    },
+    {
+        day: new Date(2018, 8, 5),
+        cat: 'coffeeshop',
+        amount: 6.5
+    },
+    {
+        day: new Date(2018, 8, 5),
+        cat: 'transport',
+        amount: 19.9,
+        comment: 'train ticket Leipzig -> Berlin'
+    },
+    {
+        day: new Date(2018, 8, 5),
+        cat: 'culture',
+        amount: 16,
+        comment: 'anual library membership fees'
+    },
+    {
+        day: new Date(2018, 8, 5),
+        cat: 'gifts',
+        amount: 37.80,
+        comment: 'christmas presents'
+    },
+    {
+        day: new Date(2018, 8, 5),
+        cat: 'culture',
+        amount: 9.99,
+        comment: 'book'
+    },
+    {
+        day: new Date(2018, 8, 3),
+        cat: 'culture',
+        amount: 7.5,
+        comment: 'cinema ticket'
+    },
+
+];
+
+
+export default spendings;
