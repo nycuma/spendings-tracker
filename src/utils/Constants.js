74ac926c603d9757e4cbf165a3570d2082816ca7
Juri <31799327+nycuma@users.noreply.github.com>
2018-09-08 22:00:28 +0200

    separate object for Settings

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,17 +1,36 @@
 // make constants private?
 
-let Constants = {
+
+let Settings = {
     CURRENCY: 'EUR',
-    DATE_FORMAT: 'DD MMM YYYY',
+    LOCALE_CURRENCY: 'de-DE',
     SPENDING_CATEGORIES: [
-        { label: 'Food', value: 'food' },
-        { label: 'Cafés, Bars & Restaurants', value: 'restaurants' },
-        { label: 'Household', value: 'food' },
-        { label: 'Culture', value: 'culture' },
-        { label: 'Clothes', value: 'clothes' },
-        { label: 'Transport', value: 'transport' },
-        { label: 'Gifts & Donations', value: 'gifts' }
-    ]
+        { label: 'Food', value: 'food', descr: '' },
+        { label: 'Coffee Shops, Bars & Restaurants', value: 'coffeeshop', descr: '' },
+        { label: 'Household', value: 'household', descr: '' },
+        { label: 'Culture', value: 'culture', descr: '' },
+        { label: 'Clothes', value: 'clothes', descr: '' },
+        { label: 'Transport', value: 'transport', descr: '' },
+        { label: 'Gifts & Donations', value: 'gifts', descr: '' }
+    ]   
 };
 
-export default Constants;
+
+
+const Constants = {
+    LOCALE_CURRENCY_OPTIONS: { 
+        style: 'currency', 
+        currency: Settings.CURRENCY, 
+        minimumFractionDigits: 2, 
+        maximumFractionDigits: 2 
+    },
+    LOCALE_DECIMAL_OPTIONS: { 
+        style: 'decimal', 
+        minimumFractionDigits: 2, 
+        maximumFractionDigits: 2 
+    },
+    DATE_FORMAT: 'DD MMM YYYY',
+    
+};
+
+export { Constants, Settings };
