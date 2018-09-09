4633f9dd99ab1599239b46a23e6b64a7bf520106
Juri <31799327+nycuma@users.noreply.github.com>
2018-09-09 19:00:27 +0200

    expanded view of all spending positions

--- src/spendings/SpendingsSingleDay.js
+++ src/spendings/SpendingsSingleDay.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { Constants, Settings } from '../utils/Constants';
 
 class SpendingsSingleDay extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            catExpandedView: this.initStateObject()
+        };
+    }
+
+    initStateObject() {
+        let stateExpandedView = {};
+        Settings.SPENDING_CATEGORIES.forEach((cat) => {
+            stateExpandedView[cat.value] = false;
+        });
+        return stateExpandedView;
+    }
     
     calculateTotalAmountPerCategory(cat) {
-        //console.log('spendingsForDay: ' + JSON.stringify(this.props.spendingsForDay));
         if (this.props.spendingsForDay && this.props.spendingsForDay.length > 0) {
             return this.props.spendingsForDay.filter((item) => {
                 return item.cat === cat;
@@ -18,17 +31,54 @@ class SpendingsSingleDay extends React.Component {
         return 0;  
     }
 
+    getSpendingsPositionByCat(cat) {
+        if (this.props.spendingsForDay && this.props.spendingsForDay.length > 0) {
+            return this.props.spendingsForDay.filter((item) => {
+                return item.cat === cat;
+            });
+        }
+    }
+
+    toggleDisplayAllPositionsForCat(cat) {
+        let updatedCatState = this.state.catExpandedView;
+        updatedCatState[cat] = !this.state.catExpandedView[cat];
+        this.setState({ catExpandedView: updatedCatState });
+    }
+
     renderTableBody() {
 
         return Settings.SPENDING_CATEGORIES.map((cat) => {
-            let amountSpent = this.calculateTotalAmountPerCategory(cat.value).toLocaleString(Settings.LOCALE_CURRENCY, Constants.LOCALE_CURRENCY_OPTIONS);
+            let amountSpent = this.calculateTotalAmountPerCategory(cat.value)
+                                  .toLocaleString(Settings.LOCALE_CURRENCY, Constants.LOCALE_CURRENCY_OPTIONS);
+            let arrowClass = this.state.catExpandedView[cat.value] ? 'arrow-up' : 'arrow-down';
             return (
-                <tr key={cat.value}>
-                    <td>{cat.value}</td>
-                    <td >{amountSpent}</td>
-                </tr>
+                <tbody key={cat.value}>
+                    <tr>
+                        <td>{cat.label}</td>
+                        <td >{amountSpent}</td>
+                        <td><span className={arrowClass} onClick={() => this.toggleDisplayAllPositionsForCat(cat.value)}></span></td>
+                    </tr>
+                    {this.renderSpendingsPositons(cat.value)}
+                </tbody>
             );
         });
+    }
+
+    renderSpendingsPositons(cat) {
+        let spendingPositions = this.getSpendingsPositionByCat(cat);
+        let displayExpanded = this.state.catExpandedView[cat] ? '' : 'none';
+
+        if (spendingPositions) {
+            return spendingPositions.map((item, i) => {
+                return (
+                    <tr key={cat+i} style={{display: displayExpanded}}>
+                        <td>{item.comment}</td>
+                        <td>{item.amount.toLocaleString(Settings.LOCALE_CURRENCY, Constants.LOCALE_CURRENCY_OPTIONS)}</td>
+                    </tr>
+                );
+            });
+        }
+
     }
 
     render() {
@@ -42,10 +92,7 @@ class SpendingsSingleDay extends React.Component {
                         <td>Amount</td>
                     </tr>
                 </thead>
-                <tbody>
-                    {this.renderTableBody()}
-                </tbody>
-            
+                {this.renderTableBody()}          
             </table>
 
         ); 
