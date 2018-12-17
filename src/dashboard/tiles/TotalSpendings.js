123c4e8fb954b13fae510bde92e2b58402efa221
Juri <31799327+nycuma@users.noreply.github.com>
2018-12-17 18:22:49 +0100

    changed table design on dashboard tiles

--- src/dashboard/tiles/TotalSpendings.js
+++ src/dashboard/tiles/TotalSpendings.js
@@ -9,20 +9,20 @@ function TotalSpendings(props) {
             <h4>Total Spendings</h4>
             <table className="table-spendings">
                 <tbody>
-                        <tr>
-                        <td>...this day:</td>
+                    <tr>
+                        <td>This day:</td>
                         <td className="cell-amount">{props.totalAmountDay.toLocaleString(Settings.LOCALE_CURRENCY, Constants.LOCALE_CURRENCY_OPTIONS)}</td>
                     </tr>
                     <tr>
-                        <td>...this week:</td>
+                        <td>This week:</td>
                         <td className="cell-amount">{props.totalAmountWeek.toLocaleString(Settings.LOCALE_CURRENCY, Constants.LOCALE_CURRENCY_OPTIONS)}</td>
                     </tr>
                     <tr>
-                        <td>...this month:</td>
+                        <td>This month:</td>
                         <td className="cell-amount">{props.totalAmountMonth.toLocaleString(Settings.LOCALE_CURRENCY, Constants.LOCALE_CURRENCY_OPTIONS)}</td>
                     </tr>
                     <tr>
-                        <td>...this year:</td>
+                        <td>This year:</td>
                         <td className="cell-amount">{props.totalAmountYear.toLocaleString(Settings.LOCALE_CURRENCY, Constants.LOCALE_CURRENCY_OPTIONS)}</td>
                     </tr>
                 </tbody>
