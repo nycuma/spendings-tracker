b89288027da5bfdb47b309b48eeb52769999a918
Juri <31799327+nycuma@users.noreply.github.com>
2019-01-21 17:46:36 +0100

    add colors to pie chart

--- src/dashboard/tiles/CategoriesPieChart.js
+++ src/dashboard/tiles/CategoriesPieChart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PieChart, Pie, Legend, Tooltip } from 'recharts';
+import { PieChart, Pie, Legend, Cell } from 'recharts';
 import { Settings } from '../../utils/Constants';
 import './Tiles.css';
 
@@ -13,8 +13,10 @@ class CategoriesPieChart extends React.Component {
 
     render() {
         const randomData = [35,20,30,15,10,5,5]; // TODO get data
+        const pieColors = [];
         const chartData = Settings.SPENDING_CATEGORIES.map((cat, i) => {
-            return { name: cat.label, value: randomData[i] }; // TODO add colors
+            pieColors.push(cat.color);
+            return { name: cat.label, value: randomData[i] };
         });
         
         return (
@@ -22,7 +24,11 @@ class CategoriesPieChart extends React.Component {
                 <h4>Spendings on Categories</h4>
                 <PieChart width={450} height={150}>
                     <Pie isAnimationActive={true} data={chartData} outerRadius={40} fill="#58747E" 
-                        labelLine={true} label dataKey="value" nameKey="name"/>
+                        labelLine={true} label dataKey="value" nameKey="name">
+                        {
+          	                chartData.map((entry, i) => <Cell fill={pieColors[i]}/>)
+                        }
+                     </Pie>   
                     <Legend layout="vertical" align="right" width="100" iconType="square" iconSize="12"/>
                 </PieChart>  
             </div>
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,22 +1,17 @@
-// make constants private?
-
-
 let Settings = {
     CURRENCY: 'EUR',
     LOCALE_CURRENCY: 'de-DE',
     FIRST_DAY_WEEK: 1, //0: Sunday, 1: Monday
     SPENDING_CATEGORIES: [
-        { label: 'Food', value: 'food', descr: '' },
-        { label: 'Coffee Shops, Bars & Restaurants', value: 'coffeeshop', descr: '' },
-        { label: 'Household', value: 'household', descr: '' },
-        { label: 'Culture', value: 'culture', descr: '' },
-        { label: 'Clothes', value: 'clothes', descr: '' },
-        { label: 'Transport', value: 'transport', descr: '' },
-        { label: 'Gifts & Donations', value: 'gifts', descr: '' }
+        { label: 'Food', value: 'food', descr: '', color: '#7E6258' },
+        { label: 'Coffee Shops, Bars & Restaurants', value: 'coffeeshop', descr: '', color: '#3EAC9A' },
+        { label: 'Household', value: 'household', descr: '', color: '#AC633E' },
+        { label: 'Culture', value: 'culture', descr: '', color: '#58747E' },
+        { label: 'Clothes', value: 'clothes', descr: '', color: '#3EAC63' },
+        { label: 'Transport', value: 'transport', descr: '', color: '#7E7558' },
+        { label: 'Gifts & Donations', value: 'gifts', descr: '', color: '#AC9A3E' }
     ]   
 };
-
-
 
 const Constants = {
     LOCALE_CURRENCY_OPTIONS: { 
@@ -31,8 +26,7 @@ const Constants = {
         maximumFractionDigits: 2 
     },
     DATE_FORMAT: 'DD MMM YYYY',
-    DATE_FORMAT_INPUT: 'YYYY-MM-DD',
-    
+    DATE_FORMAT_INPUT: 'YYYY-MM-DD'
 };
 
 export { Constants, Settings };
