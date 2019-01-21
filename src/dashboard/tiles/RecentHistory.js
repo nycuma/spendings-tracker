37a2596ca8ca503b8f66365a45769c9dffc78a1f
Juri <31799327+nycuma@users.noreply.github.com>
2019-01-21 22:13:21 +0100

    implement table to view spendings recently added

--- src/dashboard/tiles/RecentHistory.js
+++ src/dashboard/tiles/RecentHistory.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import dateFnsFormat from 'date-fns/format';
+import { Constants, Settings } from '../../utils/Constants';
 import './Tiles.css';
 
 function RecentHistory(props) {
+    const entries = props.recentSpendings.map(pos => {
+        return (
+            <tr>
+                <td> {pos.dateAdded}:</td>
+                <td> {pos.comment} </td>
+                <td className="table-row-single-spendings-pos">
+                    ({pos.amount.toLocaleString(Settings.LOCALE_CURRENCY, Constants.LOCALE_CURRENCY_OPTIONS)} 
+                        spent on {dateFnsFormat(pos.day, Constants.DATE_FORMAT)})
+                </td>
+            </tr>
+        );
+    });
 
     return (
         <div className="tile">
-            <h4>Spendings History</h4>
+            <h4>Spendings History2</h4>
+            <table className="table-spendings-history">
+                <tbody>
+                    {entries}
+                </tbody>
+            </table>
         </div>
-
     );
 }
 
