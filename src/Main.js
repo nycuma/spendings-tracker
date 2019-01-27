03b1757d37bb9156ca5319ce939e7b0101a16d38
Juri <31799327+nycuma@users.noreply.github.com>
2019-01-27 23:24:46 +0100

    removed state from Main component

--- src/Main.js
+++ src/Main.js
@@ -7,35 +7,21 @@ import Settings from './Settings';
 import Sidebar from './Sidebar';
 import './style.css';
 
-class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.switchMenuItem = this.switchMenuItem.bind(this);
-        this.state = {
-            activeMenuItem: 'Dashboard'
-        };
-    }
-
-    switchMenuItem(navItem) {
-        this.setState({ activeMenuItem : navItem });
-    }
-
-    render() {
-        return (
-            <div id="main-container"> 
-                <Header />
-                <nav className="box">
-                    <Sidebar />
-                </nav> 
-                <Switch>
-                    <Route exact path='/' component={Dashboard}/>
-                    <Route path='/dashboard' component={Dashboard}/>
-                    <Route path='/analytics' component={Analytics}/>
-                    <Route path='/settings' component={Settings}/>
-                </Switch>
-            </div>
-        );
-    }
+function Main() {
+    return (
+        <div id="main-container"> 
+            <Header />
+            <nav className="box">
+                <Sidebar />
+            </nav> 
+            <Switch>
+                <Route exact path='/' component={Dashboard}/>
+                <Route path='/dashboard' component={Dashboard}/>
+                <Route path='/analytics' component={Analytics}/>
+                <Route path='/settings' component={Settings}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default Main;
