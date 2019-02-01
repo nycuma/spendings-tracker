4dfd208ffdee00492fe9f63dc052236b3a4b0b5a
Juri <31799327+nycuma@users.noreply.github.com>
2019-02-01 20:15:50 +0100

    Settings: update preferred currency

--- src/Settings.js
+++ src/Settings.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import { Constants, prefs } from './utils/Constants';
+import './settings/TilesSettings.css';
 
-class Settings extends React.Component {
+class Settings extends React.Component { // TODO change currency, spendings cats, 
+    constructor(props) {
+        super(props);
+        this.state = {
+            currency: ''
+        };
+    }
+
+    componentDidMount() {
+        this.setState({ currency: prefs.currency });
+    }
+
+    componentWillUnmount() {
+        prefs.currency = this.state.currency;
+    }
+
+    updateCurrency(e) {
+        this.setState({ currency: e.target.value });
+    }
+
     render() {
+        let currencyOptions = Constants.CURRENCIES.map(curr => 
+            (<option key={curr.code} value={curr.code}>{curr.name} ({curr.symbol})</option>));
+    
         return (
             <div id="settings" className="box">
                 <h1 className="menu-item-headline">Settings</h1>
-                TODO
+                    <div className="tileSetting">
+                        <h4>General Settings</h4>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td><h5>Currency</h5></td>
+                                    <td><span className="font-small-colored">{this.state.currency}</span></td>
+                                    <td>
+                                        Update: <select value={this.state.currency} onChange={(e) => this.updateCurrency(e)}>{currencyOptions}</select>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>  
+                    </div>
+
+                    <div className="tileSetting">
+                        <h4>Recurrent Spendings</h4>
+                    </div>
 
             </div>
         );
