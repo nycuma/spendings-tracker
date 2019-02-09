7a0a98500f4d9daf8eff16eafe1f7abde1ebea3b
Juri <31799327+nycuma@users.noreply.github.com>
2019-02-09 22:57:01 +0100

    adding context for preferences part I

--- src/Main.js
+++ src/Main.js
@@ -5,42 +5,84 @@ import Dashboard from './Dashboard';
 import Analytics from './Analytics';
 import Settings from './Settings';
 import Sidebar from './Sidebar';
-import { loadCurrency, loadLocale, loadCategories } from './utils/LocalStore';
-import { prefs } from './utils/Constants';
+import { loadCurrency, loadLocale, loadCategories, 
+    saveCurrency, saveLocale, saveCategories } from './utils/LocalStore';
+import { Constants } from './utils/Constants';
+import { PreferenceContext } from './utils/Contexts';
 import './style.css';
 
 class Main extends Component {
     constructor(props) {
         super(props);
-        let storedCurrency = loadCurrency();
-        if(storedCurrency) {
-            prefs.currency = storedCurrency;
-        }
+        this.updateCurrency = this.updateCurrency.bind(this);
+        this.updateLocale = this.updateLocale.bind(this);
+        this.addCategory = this.addCategory.bind(this);
+        this.removeCategory = this.removeCategory.bind(this);
 
-        let storedLocale = loadLocale();
-        if(storedLocale) {
-            prefs.locale = storedLocale;
-        }
+        let storedCurrency = loadCurrency() || Constants.DEFAULT_CURRENCY;
+        let storedLocale = loadLocale() || Constants.DEFAULT_LOCALE;
+        let storedCats = loadCategories() || Constants.DEFAULT_CATEGORIES;
 
-        let storedCats = loadCategories();
-        if(storedCats) {
-            prefs.spendingCats = storedCats;
-        }
+        this.state = {
+            currency: storedCurrency,
+            locale: storedLocale,
+            categories: storedCats
+        };
+
+        this.currencyOptions = {
+            style: 'currency',
+            currency: this.state.currency,
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+        }; 
+    }
+
+    componentWillUnmount() {
+        saveCurrency(this.state.currency);
+        saveLocale(this.state.locale);
+        saveCategories(this.state.categories);
+    }
+
+    updateCurrency(e) {
+        this.setState({ currency: e.target.value });
+    }   
+
+    updateLocale(e) {
+        this.setState({ locale: e.target.value });
+    }  
+
+    addCategory(newVal) {
+        this.setState({ categories: [...this.state.categories, newVal] });
+    }
+
+    removeCategory(catValue) {
+        let index = this.state.categories.findIndex(cat => cat.value === catValue);
+        let copy = this.state.categories.slice();
+        copy.splice(index, 1);
+        this.setState({ categories: copy });
     }
 
     render() {
+
         return (
         <div id="main-container"> 
             <Header />
             <nav className="box">
                 <Sidebar />
-            </nav> 
-            <Switch>
-                <Route exact path='/' component={Dashboard}/>
-                <Route path='/dashboard' component={Dashboard}/>
-                <Route path='/analytics' component={Analytics}/>
-                <Route path='/settings' component={Settings}/>
-            </Switch>
+            </nav>
+            <PreferenceContext.Provider value={{
+                currency: this.state.currency,
+                locale: this.state.locale,
+                updateCurrency: this.updateCurrency,
+                updateLocale: this.updateLocale
+            }}>
+                <Switch>
+                    <Route exact path='/' component={Dashboard}/>
+                    <Route path='/dashboard' component={Dashboard}/>
+                    <Route path='/analytics' component={Analytics}/>
+                    <Route path='/settings' component={Settings}/>
+                </Switch>
+            </PreferenceContext.Provider>
         </div>
         );
     }
--- src/Settings.js
+++ src/Settings.js
@@ -1,41 +1,9 @@
 import React from 'react';
-import { Constants, prefs } from './utils/Constants';
-import { saveCurrency, saveLocale, saveCategories } from './utils/LocalStore';
+import { Constants } from './utils/Constants';
+import { PreferenceContext } from './utils/Contexts';
 import './settings/TilesSettings.css';
 
 class Settings extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currency: prefs.currency,
-            locale: prefs.locale,
-            categories: prefs.spendingCats
-        };
-    }
-
-    componentWillUnmount() {
-        prefs.currency = this.state.currency;
-        prefs.locale = this.state.locale;
-        prefs.spendingCats = this.state.categories;
-        saveCurrency(this.state.currency);
-        saveLocale(this.state.locale);
-        saveCategories(this.state.categories);
-    }
-
-    updateCurrency(e) {
-        this.setState({ currency: e.target.value });
-    }
-
-    updateLocale(e) {
-        this.setState({ locale: e.target.value });
-    }
-
-    removeCategory(catValue) {
-        let index = this.state.categories.findIndex(cat => cat.value === catValue);
-        let copy = this.state.categories.slice();
-        copy.splice(index, 1);
-        this.setState({ categories: copy });
-    }
 
     render() {
         let currencyOptions = Constants.CURRENCIES.map(curr => 
@@ -43,41 +11,46 @@ class Settings extends React.Component {
 
         let localeOptions = Constants.LOCALES.map(loc => (<option key={loc} value={loc}>{loc}</option>));
 
-        let categories = this.state.categories.map(cat => (
-            <tr className="reducedPadding" key={cat.value}>
-                <td><span className="font-small-colored">{cat.label}</span></td>
-                <td><button className="btnDelete" title="Delete" onClick={() => this.removeCategory(cat.value)}>X</button></td>
-            </tr>));
+        // let categories = this.state.categories.map(cat => (
+        //     <tr className="reducedPadding" key={cat.value}>
+        //         <td><span className="font-small-colored">{cat.label}</span></td>
+        //         <td><button className="btnDelete" title="Delete" onClick={() => this.removeCategory(cat.value)}>X</button></td>
+        //     </tr>));
     
         return (
             <div id="settings" className="box">
                 <h1 className="menu-item-headline">Settings</h1>
                     <div className="tileSetting">
                         <h4>Preferences</h4>
-                        <table>
+
+                        <PreferenceContext.Consumer>
+                        {({currency, locale, updateCurrency, updateLocale }) => (
+                            <table>
                             <tbody>
                                 <tr>
                                     <td className="prefTitle">Currency</td>
-                                    <td><span className="font-small-colored">{this.state.currency}</span></td>
+                                    <td><span className="font-small-colored">{currency}</span></td>
                                     <td>
-                                        Update: <select value={this.state.currency} onChange={(e) => this.updateCurrency(e)}>{currencyOptions}</select>
+                                        Update: <select value={currency} onChange={(e) => updateCurrency(e)}>{currencyOptions}</select>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td className="prefTitle">Locale</td>
-                                    <td><span className="font-small-colored">{this.state.locale}</span></td>
+                                    <td><span className="font-small-colored">{locale}</span></td>
                                     <td>
-                                        Update: <select value={this.state.locale} onChange={(e) => this.updateLocale(e)}>{localeOptions}</select>
+                                        Update: <select value={locale} onChange={(e) => updateLocale(e)}>{localeOptions}</select>
                                     </td>
                                 </tr>
-                                <tr>
+                                {/* <tr>
                                     <td className="prefTitle" rowSpan={this.state.categories.length + 1}>Categories</td>
                                 </tr>
-                                {categories}
+                                {categories} */}
                             </tbody>
-                        </table>  
+                        </table> 
+                        )}
+                        </PreferenceContext.Consumer>
+                  
                     </div>
-
                     <div className="tileSetting">
                         <h4>Recurrent Spendings</h4>
                     </div>
