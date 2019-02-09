ac5c5616748148b4cac36e01dc25d80254d5da57
Juri <31799327+nycuma@users.noreply.github.com>
2019-02-09 22:54:06 +0100

    added some default values

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -74,11 +74,22 @@ export const Constants = {
     DATE_FORMAT: 'DD MMM YYYY',
     DATE_FORMAT_SHORT: 'DD.MM.YYYY',
     DATE_FORMAT_INPUT: 'YYYY-MM-DD',
+    FIRST_DAY_WEEK: 1, // 0: Sunday, 1: Monday
 
     CURRENCIES: [
         { name: 'Euro', code: 'EUR', symbol: '€' },
         { name: 'US Dollar', code: 'USD', symbol: '$' },
         { name: 'British Pound', code: 'GBP', symbol: '£' }
     ],
-    LOCALES: ['de-DE', 'en-US', 'en-UK']
+    LOCALES: ['de-DE', 'en-US', 'en-UK'],
+
+    DEFAULT_LOCALE: 'de-DE',
+    DEFAULT_CURRENCY: 'EUR',
+    DEFAULT_CATEGORIES: [{ label: 'Food', value: 'food', descr: '', color: '#7E6258' },
+        { label: 'Coffee Shops, Bars & Restaurants', value: 'coffeeshop', descr: '', color: '#3EAC9A' },
+        { label: 'Household', value: 'household', descr: '', color: '#AC633E' },
+        { label: 'Culture', value: 'culture', descr: '', color: '#58747E' },
+        { label: 'Clothes', value: 'clothes', descr: '', color: '#3EAC63' },
+        { label: 'Transport', value: 'transport', descr: '', color: '#7E7558' },
+        { label: 'Gifts & Donations', value: 'gifts', descr: '', color: '#AC9A3E' }]
 };
