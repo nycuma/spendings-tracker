95b210aac758f07b6ac246c0a8a25bd2ebcc9a47
Juri <31799327+nycuma@users.noreply.github.com>
2019-02-22 19:19:02 +0100

    Updates

---
+++ precache-manifest.0d59f73a8f5ac390b9a45af1fddef27c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e02f6d94bc7da100a33c59e0e846a079",
+    "url": "/spendings-tracker/static/media/rubik-regular-webfont.e02f6d94.woff"
+  },
+  {
+    "revision": "a3f6ffca4553b1c8afa4fa319c379632",
+    "url": "/spendings-tracker/static/media/rubik-regular-webfont.a3f6ffca.woff2"
+  },
+  {
+    "revision": "35af6dbe749b9704f03282e5a5949253",
+    "url": "/spendings-tracker/static/media/Rubik-Regular.35af6dbe.ttf"
+  },
+  {
+    "revision": "febd9e64ba5a127ffa8e",
+    "url": "/spendings-tracker/static/js/runtime~main.febd9e64.js"
+  },
+  {
+    "revision": "f7210622a5eb1b9ad8a3",
+    "url": "/spendings-tracker/static/js/main.f7210622.chunk.js"
+  },
+  {
+    "revision": "9531348dd2cd8fb91f0a",
+    "url": "/spendings-tracker/static/js/2.9531348d.chunk.js"
+  },
+  {
+    "revision": "f7210622a5eb1b9ad8a3",
+    "url": "/spendings-tracker/static/css/main.7e702e1c.chunk.css"
+  },
+  {
+    "revision": "9531348dd2cd8fb91f0a",
+    "url": "/spendings-tracker/static/css/2.6ba9cccb.chunk.css"
+  },
+  {
+    "revision": "7989159dc81b24459d3074ac9a24bf88",
+    "url": "/spendings-tracker/index.html"
+  }
+];
